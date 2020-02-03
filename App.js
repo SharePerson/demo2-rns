870ceb363a7d8889e47d00d9ce82726dc4292811
Fady Soliman <fadysoliman@Fadys-MacBook-Air.local>
2020-02-03 03:12:30 +0200

    adding react native swiper screen

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
+
 import HomeScreen from './src/Screens/HomeScreen';
+import ReactNativeSwiper from './src/Screens/Plugins/ReactNativeSwiper';
 
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
+    RNSwiper: ReactNativeSwiper,
   },
   {
     initialRouteName: 'Home',
--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,12 +1,23 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Button, StyleSheet} from 'react-native';
 
 export default class HomeScreen extends Component {
   render() {
     return (
-      <View>
-        <Text>This is HomeScreen</Text>
+      <View style={styles.mainContainer}>
+        <Button
+          title="React Native Swiper Plugin"
+          onPress={() => this.props.navigation.navigate('RNSwiper')}
+        />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
