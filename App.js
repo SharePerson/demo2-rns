8c835c366f14ffe022072866e56ae003da902812
Fady Soliman <fadysoliman@Fadys-MacBook-Air.local>
2020-02-03 02:57:18 +0200

    test

---
+++ App.js
@@ -0,0 +1,17 @@
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+import HomeScreen from './src/Screens/HomeScreen';
+
+const navigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+  },
+  {
+    initialRouteName: 'Home',
+    defaultNavigationOptions: {
+      title: 'React Native Swiper',
+    },
+  },
+);
+
+export default createAppContainer(navigator);
