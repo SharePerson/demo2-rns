00a293001d66d6592118fb1a19504e4bcb1c6d8e
Fady Soliman <fadysoliman@Fadys-MacBook-Air.local>
2020-02-05 23:39:42 +0200

    adjusting shadow settings

--- src/Screens/Plugins/ReactNativeSwipeable.js
+++ src/Screens/Plugins/ReactNativeSwipeable.js
@@ -58,11 +58,11 @@ const styles = StyleSheet.create({
   itemContainer: {
     backgroundColor: '#fff',
     shadowColor: '#000',
-    shadowOffset: {width: 0, height: 2},
-    shadowOpacity: 0.8,
-    shadowRadius: 2,
+    shadowOffset: {width: 0, height: 1},
+    shadowOpacity: 0.5,
+    shadowRadius: 1,
     elevation: 1,
-    marginVertical: 5,
+    marginVertical: 3,
   },
   text: {
     marginLeft: 10,
